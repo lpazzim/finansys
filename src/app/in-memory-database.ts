0b2a40dc6124412f9c5fdf957b9201ed34f42a03
Lucas Pazzim <lpazzim@gmail.com>
2021-01-10 13:44:10 -0300

    Category module

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,15 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Category } from "./pages/categories/shared/category.model";
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb() {
+    const categories: Category[] = [
+      { id: 1, name: 'Moradia', description: 'Pagamentos de contas da casa' },
+      { id: 2, name: 'Saúde', description: 'Plano de saúde e remédios' },
+      { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
+      { id: 4, name: 'Salário', description: 'Recebimento de Salario' },
+      { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer' }
+    ];
+    return { categories }
+  }
+}
