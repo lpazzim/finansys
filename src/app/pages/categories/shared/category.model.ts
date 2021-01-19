4720c19c64773d41ff5087794a1812d9da952556
Lucas Pazzim <lpazzim@gmail.com>
2021-01-19 14:44:06 -0300

    adding BaseResourceModel

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,7 +1,11 @@
-export class Category {
+import { BaseResourceModel } from '../../../shared/models/base-resourse-model';
+
+export class Category extends BaseResourceModel {
   constructor(
-    public id?:number,
+    public id?: number,
     public name?: string,
     public description?: string
-  ){}
+  ) {
+    super();
+  }
 }
